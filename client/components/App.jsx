6ddda5aa4d393c287a8ce99e337ab4dbb4816300
Milou Post <post.milou@gmail.com>
2017-07-17 13:50:54 +0200

    implement material ui and add login button

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
+
+injectTapEventPlugin();
 
 export default class App extends React.Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+      <MuiThemeProvider>
+        <div style={{textAlign: 'center'}}>
+          <h1>Hello World</h1>
+          <RaisedButton
+            label={'Login'}
+            secondary={true}
+            onTouchTap={()=>{console.log('hello i work')}}
+          />
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
