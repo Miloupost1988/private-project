025c4b532862d6dff07759dcb9dd5cb39f129279
Milou Post <post.milou@gmail.com>
2017-07-17 09:27:43 +0200

    integrate react router and add linkedin sdk

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import routes from '../routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
---
+++ routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
+import About from './client/components/About';
+import  { Left, Right, Whoops404 } from './client/components/index';
+import App from './client/components/App.jsx';
+
+const routes = (
+  <Router history={hashHistory}>
+  <Route path="/" component={App} />
+  <Route path="/" component={Left}>
+  <Route path="about" component={About} />
+  </Route>
+  <Route path="*" component={Whoops404} />
+  </Router>
+)
+
+export default routes;
